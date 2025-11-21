43d5477e211e2e1c7d82a6b1d66693081b358aa2
dashaaGera <dashaagera01@gmail.com>
2025-11-21 18:35:35 +0300

    added realization functions for circle list

--- lib_algorithms/algorithms.h
+++ lib_algorithms/algorithms.h
@@ -1,8 +1,48 @@
 #include "../lib_stack/stack.h"
+#include "../lib_list/list.h"
 #include <string.h>
 #ifndef ALGORITHMS_H
 #define ALGORITHMS_H
 
 bool check_breckets(std::string s);
+
+template <typename T>
+bool has_cycle_hare_and_turtle(const List<T>& list) {
+    if (list.head() == nullptr || list.head()->next == nullptr)
+        return false;
+
+    Node<T>* slow = list.head();
+    Node<T>* fast = list.head();
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+
+    return false;
+}
+
+template<typename T>
+bool has_cycle_reverse_pointers(const List<T>& list) {
+    if (list.is_empty()) return false;
+
+    Node<T>* curr = list.head();
+    Node<T>* prev = nullptr;
+    Node<T>* next = nullptr;
+
+    while (curr != nullptr) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        if (curr == list.head()) {
+            return true;
+        }
+    }
+    return false;
+}
 
 #endif
--- tests/test_algorithms.cpp
+++ tests/test_algorithms.cpp
@@ -33,5 +33,72 @@ TEST(TestCheckBreckets, check_breckets_return_false_when_pair_of_two_not_identic
     EXPECT_EQ(check_breckets("(]"), false);
 }
 
+TEST(TestCycleList, has_cycle_hare_and_turtle_work_correctly_in_empty_list) {
+    List<int> l1;
+    EXPECT_EQ(has_cycle_hare_and_turtle(l1), false);
+}
 
+TEST(TestCycleList, has_cycle_hare_and_turtle_work_correctly_in_no_empty_list_without_cycles) {
+    List<int> l1;
+    l1.push_back(1);
+    l1.push_back(2);
+    l1.push_back(3);
+    EXPECT_EQ(has_cycle_hare_and_turtle(l1), false);
+}
+
+TEST(TestCycleList, has_cycle_hare_and_turtle_work_correctly_in_no_empty_list_with_cycles) {
+    List<int> l1;
+    l1.push_back(1);
+    l1.push_back(2);
+    l1.push_back(3);
+    l1.push_back(4);
+    l1.push_back(5);
+    l1.tail()->next = l1.head()->next->next;
+    EXPECT_EQ(has_cycle_hare_and_turtle(l1), true);
+    l1.pop_back();
+
+    List<int> l2;
+    l2.push_back(1);
+    l2.push_back(2);
+    l2.push_back(3);
+    l2.push_back(4);
+    l2.push_back(5);
+    l2.tail()->next = l2.head();
+    EXPECT_EQ(has_cycle_hare_and_turtle(l2), true);
+    l2.pop_back();
+}
+
+TEST(TestCycleList, has_cycle_reverse_pointers_work_correctly_in_empty_list) {
+    List<int> l1;
+    EXPECT_EQ(has_cycle_reverse_pointers(l1), false);
+}
+
+TEST(TestCycleList, has_cycle_reverse_pointers_work_correctly_in_no_empty_list_without_cycles) {
+    List<int> l1;
+    l1.push_back(1);
+    l1.push_back(2);
+    l1.push_back(3);
+    EXPECT_EQ(has_cycle_reverse_pointers(l1), false);
+}
+
+TEST(TestCycleList, has_cycle_reverse_pointers_work_correctly_in_no_empty_list_with_cycles) {
+    List<int> l1;
+    l1.push_back(1);
+    l1.push_back(2);
+    l1.push_back(3);
+    l1.push_back(4);
+    l1.push_back(5);
+    l1.tail()->next = l1.head()->next->next;
+    EXPECT_EQ(has_cycle_reverse_pointers(l1), true);
+
+    List<int> l2;
+    l2.push_back(1);
+    l2.push_back(2);
+    l2.push_back(3);
+    l2.push_back(4);
+    l2.push_back(5);
+    l2.tail()->next = l2.head();
+    EXPECT_EQ(has_cycle_reverse_pointers(l2), true);
+
+}
 
