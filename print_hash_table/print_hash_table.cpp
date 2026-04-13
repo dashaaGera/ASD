a91c6e15fccec7973312449e795b3c572e38deef
dashaaGera <dashaagera01@gmail.com>
2026-04-13 19:10:30 +0300

    added some changes

--- print_hash_table/print_hash_table.cpp
+++ print_hash_table/print_hash_table.cpp
@@ -2,52 +2,25 @@
 #include "../lib_hash_table_chain/hash_table_c.h"
 #include "../lib_hash_table_open_address/hash_table_oa.h"
 int main() {
-    HashTableChain<int> result;
 
-    result.insert("hash", 325);
-    result.insert("list", 47);
-    result.insert("vector", 99);
+    TVector<std::pair<std::string, int>> dict1;
+    dict1.push_back({ "hash", 10072 });
+    dict1.push_back({ "list", 13736 });
+    dict1.push_back({ "vector", 16262 });
+    dict1.push_back({ "massive", 10626 });
+    dict1.push_back({ "table", 10083 });
 
-    try {
-        result.insert("vector", 35);
-    }
-    catch (const std::logic_error& e) {
-    }
 
-    result.insert("stack", 103);
+    TVector<std::pair<std::string, int>> dict2;
+    dict2.push_back({ "table", 22727 });
+    dict2.push_back({ "vector", 20728 });
+    dict2.push_back({ "queue", 28282 });
+    dict2.push_back({ "stack", 20607 });
+    dict2.push_back({ "list", 20618 });
 
-    try {
-        result.insert("list", 36);
-    }
-    catch (const std::logic_error& e) {
-    }
 
-    result.insert("queue", 15);
-
-    std::cout << "Hash Table:" << std::endl;
+    HashTableChain<int> result = merge_dict(dict1, dict2);
     result.print(std::cout);
-
-
-    HashTableChain<std::string> table;
-    table.insert("five", "5");
-    table.insert("two", "2");
-    table.insert("eight", "8");
-    table.insert("one", "1");
-    std::cout << "Hash Table:" << std::endl;
-    table.print(std::cout);
-
-
-    HashTableOpenAddress<std::string> table1;
-    table1.insert("five", "5");
-    table1.insert("two", "2");
-    table1.insert("eight", "8");
-    table1.insert("one", "1");
-    std::cout << "Hash Table:" << std::endl;
-    table1.print(std::cout);
-
-    table1.erase("one");
-    std::cout << "Hash Table:" << std::endl;
-    table1.print(std::cout);
     return 0;
 
 
